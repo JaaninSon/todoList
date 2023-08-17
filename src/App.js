59ce7e4234c8ddde7c488e7f8ef2c75de230bc31
JaaninSon <jaan1ove@naver.com>
2023-08-17 09:09:29 +0900

    todolist 작성

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import { useState } from 'react';
 
 
 function App() {
-    const [state, setState] = useState();
+    const [todos, setTodos] = useState([]);
+    const [todo, setTodo] = useState();
+
+    console.log(todo, todos);
+
   return (
     <>
-      <input width='300' height='50' onChange={(e) => {setState(e.target.value)}} />
-        <button>추가</button>
-        <p>{state}</p>
+      <input width='300' height='50' onChange={(e) => {setTodo(e.target.value)}} />
+        <button onClick={() => {setTodos([...todos, todo])}}>추가</button>
+        <p>{todos.map((item) => {
+          return <span>{item}</span>;
+        })}</p>
     </>
   );
 }
