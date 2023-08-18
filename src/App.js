352e8716e1b6deba09ebab0a89f214448aab6034
JaaninSon <jaan1ove@naver.com>
2023-08-18 23:50:41 +0900

    삭제기능 추가

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import { useState } from 'react';
-
+import { useState } from "react";
 
 function App() {
-    const [todos, setTodos] = useState([]);
-    const [todo, setTodo] = useState();
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState();
 
-    console.log(todo, todos);
+  console.log(todo, todos);
 
   return (
     <>
-      <input width='300' height='50' onChange={(e) => {setTodo(e.target.value)}} />
-        <button onClick={() => {setTodos([...todos, todo])}}>추가</button>
-        <p>{todos.map((item) => {
-          return <span>{item}</span>;
-        })}</p>
+      <input
+        width="300"
+        height="50"
+        onChange={(e) => {
+          setTodo(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          setTodos([...todos, todo]);
+        }}
+      >
+        추가
+      </button>
+      <p>
+        {todos.map((item, index) => {
+          return (
+            <>
+              <p>{item}</p>
+              <button
+                onClick={() => {
+                  const newState = [...todos];
+                  delete newState[index];
+                  setTodos(newState.filter((todo) => todo));
+                }}
+              >
+                삭제
+              </button>
+            </>
+          );
+        })}
+      </p>
     </>
   );
 }
